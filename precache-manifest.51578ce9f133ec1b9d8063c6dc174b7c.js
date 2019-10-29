de22557969a362a94435cc0dc42e7454de757d01
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2019-10-29 07:16:44 +0300

    Updates

---
+++ precache-manifest.51578ce9f133ec1b9d8063c6dc174b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c816d5ceacd4cfbb619a509c0f8a17a4",
+    "url": "vitaliiborodulin.github.io/insta/index.html"
+  },
+  {
+    "revision": "20e969fe5957cc354b77",
+    "url": "vitaliiborodulin.github.io/insta/static/css/main.3612a5d5.chunk.css"
+  },
+  {
+    "revision": "aff25ff728cc208490ad",
+    "url": "vitaliiborodulin.github.io/insta/static/js/2.018e783a.chunk.js"
+  },
+  {
+    "revision": "20e969fe5957cc354b77",
+    "url": "vitaliiborodulin.github.io/insta/static/js/main.6f75d212.chunk.js"
+  },
+  {
+    "revision": "1475594b618dbd149b7e",
+    "url": "vitaliiborodulin.github.io/insta/static/js/runtime-main.5d46f24e.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "vitaliiborodulin.github.io/insta/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "vitaliiborodulin.github.io/insta/static/media/logo.9e8a2b39.svg"
+  }
+]);
